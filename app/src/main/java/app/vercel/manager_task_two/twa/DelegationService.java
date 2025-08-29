2bc631df1304397cdc07517fe71824f0da90ff41
Yan <crowbeat.47@gmail.com>
2025-08-29 16:34:41 +0500

    feat: init project for TWA

---
+++ app/src/main/java/app/vercel/manager_task_two/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.manager_task_two.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
